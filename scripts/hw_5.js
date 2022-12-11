db0494722ac3cf65a189cb1f97a6039f850ef281
Sakal_Julia <sakal.iuliia@gmail.com>
2022-12-11 12:31:28 +0100

    2nd lesson adj

---
+++ scripts/hw_5.js
@@ -0,0 +1,13 @@
+const stringInput1 = prompt('Enter anything'); 
+const stringInput2 = prompt('Again enter anything'); 
+const stringInput3 = prompt('And again enter anything'); 
+const stringCombo = stringInput1 + " " + stringInput2 + " " + stringInput3; 
+alert(stringCombo);
+
+const number = prompt('Enter five-digit number',0); 
+if (isNaN(number) === true || number.length !== 5) { 
+alert('Sorry, you didn\'t follow the instructions'); 
+} else { 
+const myArray = number.toString().split(''); 
+alert(`${myArray[0]} ${myArray[1]} ${myArray[2]} ${myArray[3]} ${myArray[4]}`); 
+}
